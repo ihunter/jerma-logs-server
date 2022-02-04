af49e87fb5063a18cde411ab41aaf2988fe7bcc5
ihunter <ijh.hunter@gmail.com>
2022-02-03 22:54:15 -0500

    group stored messages util

---
+++ src/utils/groupMessageByYearAndMonth.js
@@ -0,0 +1,9 @@
+const { groupStoredMessagesByYearAndMonth } = require('./index')
+
+groupStoredMessagesByYearAndMonth()
+  .then(() => {
+    console.log('Done')
+  })
+  .catch(err => {
+    console.log(err)
+  })
