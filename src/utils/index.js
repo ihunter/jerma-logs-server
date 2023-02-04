dfd798676b65a351c9c7b2c56af7655b0468e9cc
ihunter <ijh.hunter@gmail.com>
2023-02-03 21:06:49 -0500

    remove console logs

--- src/utils/index.js
+++ src/utils/index.js
@@ -28,12 +28,8 @@ exports.formatMessage = (tags, message) => {
 };
 
 exports.logSus = async (messageData) => {
-  console.log("Possible sus update")
   const susRegExp = new RegExp(/^!(commands\sedit|editcom)\s(-(cd|ul|a)=\w+\s)*?!sus\s.+/);
-  if (messageData.message.match(susRegExp) === null) {
-    console.log("No sus message found with regex:", messageData.message)
-    return
-  };
+  if (messageData.message.match(susRegExp) === null) return
 
   const susCollectionRef = db.collection("sus");
 
