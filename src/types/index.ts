785ecf716bb60fa391d05af3ae146cf9210e5fcc
ihunter <ijh.hunter@gmail.com>
2025-07-15 02:21:26 -0400

    update tsconfig and adjust types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,29 +1,27 @@
 import type tmi from 'tmi.js'
 
 export interface MessageData {
-  id?: string
+  id?: string | undefined
   message?: string
-  sentAt?: string
+  sentAt?: string | undefined
 
-  badgeInfo?: tmi.BadgeInfo
-  badgeInfoRaw?: string
-  badges?: tmi.Badges
-  badgesRaw?: string
-  color?: string
-  displayName?: string
-  emotes?: {
-    [emoteid: string]: string[]
-  }
-  emotesRaw?: string
-  flags?: string
-  messageType?: string
-  mod?: boolean
-  roomID?: string
-  subscriber?: boolean
-  turbo?: boolean
-  userID?: string
-  userType?: string
-  username?: string
+  badgeInfo?: tmi.BadgeInfo | undefined
+  badgeInfoRaw?: string | undefined
+  badges?: tmi.Badges | undefined
+  badgesRaw?: string | undefined
+  color?: string | undefined
+  displayName?: string | undefined
+  emotes?: { [emoteid: string]: string[] } | undefined
+  emotesRaw?: string | undefined
+  flags?: string | undefined
+  messageType?: string | undefined
+  mod?: boolean | undefined
+  roomID?: string | undefined
+  subscriber?: boolean | undefined
+  turbo?: boolean | undefined
+  userID?: string | undefined
+  userType?: string | undefined
+  username?: string | undefined
   reply?: {
     parent: {
       displayName: string
